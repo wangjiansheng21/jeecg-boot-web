82015e989f4c83826ebbb394d33295929434f49d
wangjiansheng21 <1823079199@qq.com>
2020-12-08 00:08:53 +0800

    创建分支

--- vue.config.js
+++ vue.config.js
@@ -87,7 +87,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 3000,
+    port: 8080,
     proxy: {
      /* '/api': {
         target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro', //mock API接口系统
